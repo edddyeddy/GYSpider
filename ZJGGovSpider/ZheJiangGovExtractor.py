f811f7c598e4b03c4a43b86710c60c386ee7bae0
edddyeddy <ziyouer@live.com>
2024-07-21 22:40:47 +0800

    update

--- ZJGGovSpider/ZheJiangGovExtractor.py
+++ ZJGGovSpider/ZheJiangGovExtractor.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import re
 
 class ZhejiangGovExtractor(object):
     """
@@ -14,8 +15,19 @@ class ZhejiangGovExtractor(object):
         data = self.__ZhejiangGovInfo()
         
         data['title'] = soup.find(attrs={"name": "ArticleTitle"})['content']
-        data['pubDate'] = soup.find(attrs={"name": "PubDate"})[
-            'content'].split(' ')[0]
+        # data['pubDate'] = soup.find(attrs={"name": "PubDate"})[
+        #     'content'].split(' ')[0]
+        # 规章要从内容中提取
+        subTitleSoup = soup.find(attrs={"class": "zc_artice_tit1"})
+        subTitle = soup.text
+        date_pattern = r'(\d{4})年(\d{1,2})月(\d{1,2})日'
+        match = re.search(date_pattern, subTitle)
+        year = match.group(1)
+        month = match.group(2).zfill(2)  # 将月份填充为两位数
+        day = match.group(3).zfill(2)    # 将日期填充为两位数
+        formatted_date = f"{year}-{month}-{day}"
+        data['pubDate'] = formatted_date
+        
         data['url'] = rowData['projectID']
         data['column'] = soup.find(attrs={"name": "ColumnName"})['content']
         contentSoup = soup.find(attrs={"class": "zc_article_con"})
--- ZhejiangGov.py
+++ ZhejiangGov.py
@@ -4,11 +4,11 @@ import pymongo
 if __name__ == "__main__":
     client = pymongo.MongoClient("mongodb://localhost:27017/")
     db = client["GYDatabase"]
-    col = db['浙江省_其他文件']
+    col = db['浙江省_规章']
     
     spider = ZhejiangGovSpider(col)
     
-    # rowData = spider._getRowData('https://www.zj.gov.cn/art/2005/1/13/art_1229591319_64508.html')
+    # rowData = spider._getRowData('https://www.zj.gov.cn/art/2024/2/1/art_1229604638_2512021.html')
     # data = spider._extractData(rowData)
     # print(data)
     
@@ -16,4 +16,4 @@ if __name__ == "__main__":
     # print(len(urls))
     # print(urls)
     
-    spider.scrapeAllProjectDataByPage(1, 133)
+    spider.scrapeAllProjectDataByPage(1, 17)
