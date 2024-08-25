aa12339b029f622eca5f1f5132a719c2bae89f67
edddyeddy <ziyouer@live.com>
2024-08-25 12:36:37 +0800

    update

--- SXGovSpider/ShanXiGovExtractor.py
+++ SXGovSpider/ShanXiGovExtractor.py
@@ -16,18 +16,9 @@ class ShanxiGovExtractor(object):
         data['title'] = soup.find(attrs={"name": "ArticleTitle"})['content']
         data['pubDate'] = soup.find(attrs={"name": "PubDate"})['content'].split(' ')[0]
         data['url'] = rowData['projectID']
-        contentSoup = None
-        ## 行政规范文件
-        # # contentSoup = soup.find(attrs={"class": "sxgzk-detail-con"})
-        # 省政府文件
-        # if contentSoup is None:
-        #     contentSoup = soup.find(attrs={"class": "view TRS_UEDITOR trs_paper_default trs_web"})
-        # 废止省政府办公厅
+        contentSoup = soup.find(attrs={"class": "sxgzk-detail-con"})
         if contentSoup is None:
-            contentSoup = soup.find(attrs={"class": "view TRS_UEDITOR trs_paper_default trs_external trs_web"})
-        # 废止省政府
-        if contentSoup is None:
-            contentSoup = soup.find(attrs={"class": "view TRS_UEDITOR trs_paper_default trs_external trs_web trs_word"})
+            contentSoup = soup.find(attrs={"class": "container boxcenter sxszf-detail"})
         data['content'] = contentSoup.text
         
         
